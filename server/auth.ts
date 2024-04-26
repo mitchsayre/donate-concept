7d092b2e7934d356f32273c6fb5ce1fc6e9f3ff8
Mitchell Sayre <sayremitch@gmail.com>
2024-04-25 22:04:22 -0400

    Add Cognito password reset and verify access token

--- server/auth.ts
+++ server/auth.ts
@@ -1,8 +1,14 @@
 import {
   AdminInitiateAuthCommand,
+  AssociateSoftwareTokenCommand,
   AuthFlowType,
+  ChallengeName,
+  ChallengeNameType,
   CognitoIdentityProvider,
+  RespondToAuthChallengeCommandInput,
+  VerifySoftwareTokenCommand,
 } from "@aws-sdk/client-cognito-identity-provider";
+import { CognitoJwtVerifier } from "aws-jwt-verify";
 
 const AWS_REGION = process.env.AWS_REGION!;
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID!;
@@ -16,7 +22,7 @@ import {
   AdminGetUserCommand,
 } from "@aws-sdk/client-cognito-identity-provider";
 
-export const client = new CognitoIdentityProvider({
+const client = new CognitoIdentityProvider({
   region: AWS_REGION,
   credentials: {
     accessKeyId: AWS_ACCESS_KEY_ID,
@@ -24,18 +30,61 @@ export const client = new CognitoIdentityProvider({
   },
 });
 
-export async function getCognitoUser(username: string, password: string) {
+const verifier = CognitoJwtVerifier.create({
+  userPoolId: COGNITO_USER_POOL_ID,
+  tokenUse: "access",
+  clientId: COGNITO_CLIENT_ID,
+});
+
+export async function cognitoResetPassword(userSub: string, sessionToken: string) {
+  const input = {
+    ClientId: COGNITO_CLIENT_ID,
+    UserPoolId: COGNITO_USER_POOL_ID,
+    Session: sessionToken,
+    ChallengeName: ChallengeNameType.NEW_PASSWORD_REQUIRED,
+    ChallengeResponses: {
+      USERNAME: userSub,
+      NEW_PASSWORD: "Test1234!",
+    },
+  };
+  const res = await client.adminRespondToAuthChallenge(input);
+  console.log(res);
+}
+
+export async function cognitoLogin(userSub: string, password: string) {
   const command = new AdminInitiateAuthCommand({
     UserPoolId: COGNITO_USER_POOL_ID,
     ClientId: COGNITO_CLIENT_ID,
     AuthFlow: AuthFlowType.ADMIN_USER_PASSWORD_AUTH,
     AuthParameters: {
-      USERNAME: username,
+      USERNAME: userSub,
       PASSWORD: password,
     },
   });
 
   const response = await client.send(command);
-  console.log(response);
+  const AuthenticationResult = response.AuthenticationResult;
+  if (AuthenticationResult?.AccessToken) {
+    await cognitoVerifyAccessToken(AuthenticationResult.AccessToken);
+  }
+
+  // if (response.ChallengeName === ChallengeNameType.NEW_PASSWORD_REQUIRED) {
+
+  // }
+
+  // if (sessionToken) {
+  //   await verifySoftwareToken(sessionToken);
+  // }
+
+  // console.log(response);
   return response;
+}
+
+async function cognitoVerifyAccessToken(accessToken: string) {
+  try {
+    const payload = await verifier.verify(accessToken);
+    console.log("Token is valid. Payload:", payload);
+  } catch {
+    console.log("Token not valid!");
+  }
 }
