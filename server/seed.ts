5298105db3ef684f59dbfeef4fc4706f19f05198
Mitchell Sayre <sayremitch@gmail.com>
2024-05-09 22:21:36 -0400

    Seed refresh tokens for owners

--- server/seed.ts
+++ server/seed.ts
@@ -3,11 +3,19 @@ import { createLoaders } from "./lib/loaders";
 import { AuthMethod, Role, User } from "@prisma/client";
 import { create, db } from "./lib/database";
 import { sendSignupEmail } from "./lib/email";
-import { Session } from "./lib/bootstrap";
+import { Session, SessionToken } from "./lib/bootstrap";
+import { randomUUID } from "crypto";
+import { encrypt } from "./lib/secrets";
+import { RefreshToken } from "@prisma/client";
 
 const OWNER_EMAILS = process.env.OWNER_EMAILS!.split(",");
 const BOT_UUID = process.env.BOT_UUID!;
 const URL = process.env.URL!;
+const SESSION_ENCRYPTION_KEY = process.env.SESSION_ENCRYPTION_KEY!;
+const SIGNUP_REFRESH_TOKEN_VALIDITY_PERIOD = parseInt(
+  process.env.SIGNUP_REFRESH_TOKEN_VALIDITY_PERIOD!
+);
+const ACCESS_TOKEN_VALIDITY_PERIOD = parseInt(process.env.ACCESS_TOKEN_VALIDITY_PERIOD!);
 
 async function main() {
   const botUser: User = {
@@ -36,15 +44,39 @@ async function main() {
     loaders: createLoaders(),
   };
 
-  OWNER_EMAILS.forEach(async (email) => {
-    create("User", session, {
-      email: email,
-      role: Role.Owner,
-      passwordEncrypted: null,
-      passwordSalt: null,
-      authMethod: AuthMethod.Pending,
-      organizationId: null,
+  const owners = await Promise.all(
+    OWNER_EMAILS.map(async (email) => {
+      return create("User", session, {
+        email: email,
+        role: Role.Owner,
+        passwordEncrypted: null,
+        passwordSalt: null,
+        authMethod: AuthMethod.Pending,
+        organizationId: null,
+      });
+    })
+  );
+
+  owners.forEach(async (owner) => {
+    if (!owner) {
+      throw Error("Failed to create owner");
+    }
+
+    const signupToken: SessionToken = {
+      sub: owner.id,
+      exp: Date.now() + ACCESS_TOKEN_VALIDITY_PERIOD,
+      id: randomUUID(),
+    };
+
+    const refreshToken = await create("RefreshToken", session, {
+      userId: owner.id,
+      expiresAt: new Date(Date.now() + SIGNUP_REFRESH_TOKEN_VALIDITY_PERIOD),
+      accessTokenId: signupToken.id,
     });
+
+    const signupTokenEncrypted = encrypt(JSON.stringify(signupToken), SESSION_ENCRYPTION_KEY);
+    console.log(`Signup token for ${owner.email}: ${signupTokenEncrypted}`);
+    // TODO: Send signup email
   });
 }
 
